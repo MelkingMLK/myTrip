6bcc9136663e6b92afa3207750ac0f1ebbf782d7
Melking <alessandro.melnykam@gmail.com>
2026-03-05 10:42:07 +0100

    1.1 completed

---
+++ drive-tracker/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-900">
+      <h1 className="text-3xl font-bold text-white text-center">
+        Drive Tracker <br/> <span className="text-blue-400 text-xl">Setup completato! 🚗💨</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
